097ef9acd34b363a9e6e21eae61cccccbdb2c632
Bryan Ortega <bj_ortega@hotmail.com>
2024-03-21 10:35:59 +0100

    instalalcion de laravel y blueprint

---
+++ routes/api.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Http\Request;
+use GuzzleHttp\Psr7\Response;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArticleController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::get('/articles/{article}', [ArticleController::class, 'show'])->name('api.v1.articles.show');
+Route::get('/articles', [ArticleController::class, 'index'])->name('api.v1.articles.index');
